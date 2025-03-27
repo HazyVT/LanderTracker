d41266459de793485585c3d4fd3f7761f98316e4
Mohammed Salim <renablehots@gmail.com>
2025-03-27 22:10:45 +0400

    Update App.tsx

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,35 +1,40 @@
-import { Box, Button, Image, Text } from "@chakra-ui/react"
-import { supabase } from "./components/Database"
-import Skylander from "./components/Skylander"
-import { useEffect, useState } from "react"
+import { Box, Image, Text } from '@chakra-ui/react'
+import { supabase } from './components/Database'
+import Skylander from './components/Skylander'
+import { useEffect, useState } from 'react'
 
 function App(): JSX.Element {
   //const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
-  const [ skylanders, setSkylanders ] = useState<Skylander[]>([]);
-  
-  const getSkylanders = async () => {
-    const { data, error } = await supabase.from('skylanders').select('name, type, image, game ( name )');
+  const [skylanders, setSkylanders] = useState<Skylander[]>([])
+
+  const getSkylanders = async (): Promise<void> => {
+    const { data, error } = await supabase
+      .from('skylanders')
+      .select('name, type ( name ), image, game ( name )')
     if (error == null) {
-      const newArray : Skylander[] = [];
+      const newArray: Skylander[] = []
       data.forEach((value) => {
-        const skylander = new Skylander(value.name, value.type, value.game.name, value.image);
-        newArray.push(skylander);
+        const skylander = new Skylander(value.name, value.type.name, value.game.name, value.image)
+        newArray.push(skylander)
       })
 
-      setSkylanders(newArray);
+      setSkylanders(newArray)
     }
   }
 
   useEffect(() => {
-    getSkylanders();
+    getSkylanders()
   }, [])
 
   return (
     <Box>
       {skylanders.map((value) => {
-        return <Box key={value.name}>
-            <Image src={`https://eoakdlw8zm.ufs.sh/f/${value.link}`} />
+        return (
+          <Box key={value.name}>
+            <Text>{value.name}</Text>
+            <Text>{value.type}</Text>
           </Box>
+        )
       })}
     </Box>
   )
